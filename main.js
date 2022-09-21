0ca4c4cfee35470b184fe9edb44922507883ec73
Jeremy Martin <jeremymartin4794@gmail.com>
2022-09-21 17:43:23 -0400

    centered the background, changed background color, and added a new nona.png with background removed

--- main.js
+++ main.js
@@ -7,20 +7,25 @@ let character;
 
 function preload() {
   spriteManager.load("nona.png"); // will look in assets folder for nona.png and nona.png.json
+  //spriteManager.load("nona_background_removed.png");
   spriteManager.preloadAll();
 }
 
 function setup() {
   spriteManager.loadAll(); // load sprites
 
-  let character = new Character(10, 10, spriteManager.get("Nona"));
+  //let character = new Character(10, 10, spriteManager.get("Nona"));
+  let character = new Character(250, 200, spriteManager.get("Nona"));
   gameManager.queue(character); // add our character to the render queue
-  gameManager.queue(new Character(200, 200, spriteManager.get("Nona"))); // add a second character to the render queue
+  //gameManager.queue(new Character(200, 200, spriteManager.get("Nona"))); // add a second character to the render queue
 
   gameController.subscribe(character.keyPressed); // subscribe to gameController event bus
 
-  createCanvas(640, 480); // Canvas size adheres to 4:3 aspect ratio
-  background(255);
+  //createCanvas(640, 480); // Canvas size adheres to 4:3 aspect ratio
+  //background(12);
+  let gameCanvas = createCanvas(640, 480);
+  gameCanvas.background(100,140,160);
+  gameCanvas.center();
 }
 
 function draw(){
