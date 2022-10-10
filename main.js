cea11029087b44f862f1698fde18f4ca397f7e29
Cohen Nichols <101533757+cnichols423@users.noreply.github.com>
2022-10-09 23:42:45 -0400

    Update main.js
    added start to mouse tracking and crosshair placement
    
    Signed-off-by: Cohen Nichols <101533757+cnichols423@users.noreply.github.com>

--- main.js
+++ main.js
@@ -10,6 +10,7 @@ let gameCanvasSizey = 720;
 function preload() {
   spriteManager.load("nona.png"); // will look in assets folder for nona.png and nona.png.json
   spriteManager.load([ "shark.png", "urchant.png", "clam.png" ]);
+  spriteManager.load("crosshair.png"); // load crosshair
 
   spriteManager.preloadAll();
 }
@@ -22,8 +23,11 @@ function setup() {
   gameManager.queue(new Enemy(300, 300, spriteManager.get("Clam")));
 
   let character = new Character(250, 200, spriteManager.get("Nona"));
+  let cross = new Character(250, 225, spriteManager.get("Crosshair"));
   gameManager.queue(character); // add our character to the render queue
+  gameManager.queue(cross);
   gameController.subscribe(character); // subscribe to gameController event bus
+  gameController.subscribe(cross);
 
   // let gameCanvas = createCanvas(640, 480);
   // gameCanvas.background(100,140,160);
@@ -51,3 +55,9 @@ function keyReleased() {
 function checkBoundaries(position) {
   console.log(position);
 }
+
+// currently does not work
+function trackMouse(){
+  gameController.trackMouse(true)
+}
+
