1ddda23234941498cf48f385b68e5cfc0a1f1dc0
Struck713 <noahstruck71303@gmail.com>
2022-10-17 15:16:27 -0400

    Updated collisions with LevelManager

--- game/level.js
+++ game/level.js
@@ -39,17 +39,20 @@ class LevelManager {
         this.current.render();
     }
 
-    getTile(x, y, offsetRow, offsetColumn) {
-        return this.tiles[this.current.getTile(x, y, offsetRow, offsetColumn)];
-    }
-
     // returns a list of nearby tiles if they are collideable
     isCollideable(x, y) {
-        return [
-            !this.getTile(x, y, 0, -1).collide, 
-            !this.getTile(x, y, 0, 1).collide, 
-            !this.getTile(x, y, -1, 0).collide,
-            !this.getTile(x, y, 1, 0).collide
+
+        var tile = this.current.getTileByXY(x, y);
+        var up = this.current.getTileByRowColumn(tile.row, tile.column - 1);
+        var down = this.current.getTileByRowColumn(tile.row, tile.column + 1);
+        var left = this.current.getTileByRowColumn(tile.row - 1, tile.column);
+        var right = this.current.getTileByRowColumn(tile.row + 1, tile.column);
+
+        return [ 
+            up.checkCollisionY(y), 
+            down.checkCollisionY(y), 
+            left.checkCollisionX(x), 
+            right.checkCollisionX(x)
         ];
     }
 
@@ -71,35 +74,73 @@ class Level {
     }
 
     load(tiles) {
-        this.tileMatrix = this.tileMatrixJSON.layout;
+        let tileIndexes = this.tileMatrixJSON.layout;
         delete this.tileMatrixJSON;
 
-        console.log(this.tileMatrix);
-
+        this.tileMatrix = [];
         this.graphics = createGraphics(GameManager.CANVAS_X, GameManager.CANVAS_Y);
         for (let column = 0; column < GameManager.COLUMNS; ++column) {
+            this.tileMatrix[column] = [];
             for (let row = 0; row < GameManager.ROWS; ++row) {
-                var tile = tiles[this.tileMatrix[column][row]];
+                var tileIndex = tileIndexes[column][row];
+                var tile = tiles[tileIndex];
                 var offsetX = row * LevelManager.TILE_SIZE;
                 var offsetY = column * LevelManager.TILE_SIZE;
+
+                this.tileMatrix[column][row] = 
+                    new Tile(tileIndex,
+                        tile.collide,
+                        row, 
+                        column, 
+                        { 
+                            x: offsetX + LevelManager.TILE_SIZE_HALF, 
+                            y: offsetY + LevelManager.TILE_SIZE_HALF, 
+                        }
+                    );
+                
                 this.graphics.image(tile, offsetX, offsetY);
             }
         }
-
     }
 
     render() {
         image(this.graphics, 0, 0);
     }
 
-    getTile(x, y, offsetRow, offsetColumn) {
-        let row = floor(x / LevelManager.TILE_SIZE) + offsetRow;
+    getTileByXY(x, y) {
+        let row = floor(x / LevelManager.TILE_SIZE);
         if (GameManager.ROWS <= row) return null;
 
-        let column = floor(y / LevelManager.TILE_SIZE) + offsetColumn;
+        let column = floor(y / LevelManager.TILE_SIZE);
         if (GameManager.COLUMNS <= column) return null;
 
+        return this.getTileByRowColumn(row, column);
+    }
+
+    getTileByRowColumn(row, column) {
         return this.tileMatrix[column][row];
+    }
+}
+
+
+class Tile {
+
+    constructor(index, collide, row, column, center) {
+        this.index = index;
+        this.row = row;
+        this.collide = collide;
+        this.column = column;
+        this.center = center;
+    }
+
+    checkCollisionX(x) {
+        let distanceX = abs(this.center.x - x) - 8;
+        return !(distanceX < LevelManager.TILE_SIZE && this.collide);
+    }
+
+    checkCollisionY(y) {
+        let distanceY = abs(this.center.y - y) - 8;
+        return !(distanceY < LevelManager.TILE_SIZE && this.collide);
     }
 
 }
