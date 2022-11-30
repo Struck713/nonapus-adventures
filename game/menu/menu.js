75143250cb4e965ffa749d85a97dc98c43c42c2e
avirathod <rathodavi02@gmail.com>
2022-11-30 18:39:32 -0500

    Reformatting for optimization

--- game/menu/menu.js
+++ game/menu/menu.js
@@ -8,9 +8,7 @@ class MenuManager {
         this.current = this.menus[0];
     }
 
-    render() {
-        this.current.render();
-    }
+    render() { this.current.render(); }
 
     mouseClicked(x, y) {
         this.current.items.forEach(item => {
@@ -25,21 +23,25 @@ class MenuManager {
             else item.hovered = true;
         })
     }
-
-    start() {
-        this.active = false;
-    }
-
+    start() { this.active = false; }
 }
 
 class Menu {
+    constructor(items) { this.items = items; }
+    render() { this.items.forEach(item => item.render()) }
+}
 
-    constructor(items) {
-        this.items = items;
+// main menu
+class MainMenu extends Menu {
+
+    constructor() {
+        super([ 
+            new MenuButton((GameManager.CANVAS_X / 2), (GameManager.CANVAS_Y / 2), 150, 50, "Play", () => menuManager.start()),
+            new MenuButton((GameManager.CANVAS_X / 2), (GameManager.CANVAS_Y / 2) + 60, 150, 50, "How To Play",
+            () => alert("If you can't figure out how to play our game we don't want you playing it. Slay.")),
+            new MenuText((GameManager.CANVAS_X / 2), (GameManager.CANVAS_Y / 2) - 100, "OATMILK SIMULATOR.", 100),
+            new MenuText(55, GameManager.CANVAS_Y - 15, "© 8+1 Man Carry", 15)
+        ]);
     }
-
-    render() {
-        this.items.forEach(item => item.render());
-    }
-
+    
 }
