74a799eb51e498ae4ecf55d726c0669b890c66a1
avirathod <rathodavi02@gmail.com>
2022-12-02 13:32:09 -0500

    Removed space

--- game/menu/items.js
+++ game/menu/items.js
@@ -41,7 +41,6 @@ class MenuButton extends MenuItem {
     inBounds(x, y) {
         return Utils.inBounds(x, y, this.x - (this.width / 2), this.y - (this.height / 2), this.width, this.height);
     }
-
 }
 
 class MenuText extends MenuItem {
