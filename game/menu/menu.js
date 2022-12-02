e737e07a2256e4d67f329ee861132dd344d59be4
avirathod <rathodavi02@gmail.com>
2022-12-02 13:28:36 -0500

    Adding a comma

--- game/menu/menu.js
+++ game/menu/menu.js
@@ -40,7 +40,7 @@ class MainMenu extends Menu {
         super("Main", [ 
             new MenuButton((GameManager.CANVAS_X / 2), (GameManager.CANVAS_Y / 2), 150, 50, "Play", () => menuManager.start()),
             new MenuButton((GameManager.CANVAS_X / 2), (GameManager.CANVAS_Y / 2) + 60, 150, 50, "How To Play",
-            () => alert("If you can't figure out how to play our game we don't want you playing it. Slay.")),
+            () => alert("If you can't figure out how to play our game, we don't want you playing it. Slay.")),
             new MenuText((GameManager.CANVAS_X / 2)-53, (GameManager.CANVAS_Y / 2) - 140, "Nonapus Adventures!", 100),
             new MenuText(43, GameManager.CANVAS_Y - 17, "© 8+1 Man Carry", 15)
         ]);
